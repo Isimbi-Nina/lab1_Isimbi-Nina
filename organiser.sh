503d2868a1f633246ee725a859a1c85c5b4bf15c
Isimbi Nina <h.isimbi@alustudent.com>
2026-07-24 19:42:19 +0200

    Shell script logic

---
+++ organiser.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if directory "archive" exists, if not create it
+if [ ! -d "archive" ]; then
+    mkdir archive
+    echo "Directory 'archive' created successfully."
+fi
+
+# Timestamp generation
+timestamp=$(date +"%Y%m%d_%H%M%S")
+echo "Timestamp: $timestamp"
+
+# Rename grades.csv
+if [ -f  "grades.csv" ]; then
+    mv grades.csv archive/grades_${timestamp}.csv
+    echo "File 'grades.csv' renamed and moved to 'archive' directory."
+else
+    echo "File 'grades.csv' not found."
+fi
+
+touch grades.csv
+echo "New file 'grades.csv' created."
+
+#Logging timestamp, original file name and new file name to organiser.log
+log_file="organiser.log"
+log_entry="[$timestamp] Renamed 'grades.csv' to 'archive/grades_${timestamp}.csv'"
+echo "$log_entry" >> "$log_file"
+
+
+
+
